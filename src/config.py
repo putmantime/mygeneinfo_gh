bdc95f1a1eaea54b95bd34e6a3ccabee98b0f7d5
newgene <anewgene@yahoo.com>
2012-06-01 16:21:18 -0700

    initial commit.

---
+++ src/config.py
@@ -0,0 +1,27 @@
+DATA_SRC_SERVER = 'localhost'
+DATA_SRC_PORT = 27017
+DATA_SRC_DATABASE = 'genedoc_src'
+DATA_SRC_MASTER_COLLECTION = 'src_master'
+
+DATA_SERVER = 'localhost'
+DATA_PORT = 27017
+DATA_DATABASE = 'genedoc'
+DATA_DB_MASTER_COLLECTION = 'db_master'
+
+ES_HOST = 'localhost:9500'
+ES_INDEX_NAME = 'genedoc'
+
+species_li = ['human', 'mouse', 'rat', 'fruitfly', 'nematode', 'zebrafish', 'thale-cress', 'frog', 'pig']
+
+taxid_d = {'human': 9606,
+           'mouse': 10090,
+           'rat': 10116,
+           'fruitfly': 7227,
+           'nematode': 6239,
+           'zebrafish': 7955,
+           'thale-cress': 3702,
+           'frog': 8364,
+           'pig': 9823,
+           }
+
+DATA_FOLDER = '<path_to_data_folder>'
