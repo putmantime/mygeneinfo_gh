fbbca6fd5a59d601461936643f95717c301cb138
newgene <anewgene@yahoo.com>
2012-12-10 12:39:38 -0800

    uploading mapping for each data sources.

--- src/dataload/__init__.py
+++ src/dataload/__init__.py
@@ -2,23 +2,56 @@
 
 import copy
 import types
+import time
 import datetime
 import importlib
 from mongokit import Document, CustomType
 from utils.mongo import get_src_conn
+from utils.common import timesofar
 from config import DATA_SRC_DATABASE, DATA_SRC_MASTER_COLLECTION
 
 
-__sources__ = ['entrez.entrez_geneinfo',
-#               'ensembl_all',
-               'entrez.entrez_homologene',
-               'entrez.entrez_genesummary',
-               'entrez.entrez_accession',
-               'entrez.entrez_refseq',
-               'entrez.entrez_unigene',
-               'entrez.entrez_go',
-               'entrez.entrez_ec',
-               'entrez.entrez_retired',
+__sources__ = [
+              'entrez.entrez_geneinfo',
+#              'ensembl_all',
+              'entrez.entrez_homologene',
+              'entrez.entrez_genesummary',
+              'entrez.entrez_accession',
+              'entrez.entrez_refseq',
+              'entrez.entrez_unigene',
+              'entrez.entrez_go',
+              'entrez.entrez_ec',
+              'entrez.entrez_retired',
+
+              'ensembl.ensembl_gene',
+              'ensembl.ensembl_genomic_pos',
+              'ensembl.ensembl_pdb',
+              'ensembl.ensembl_ipi',
+              'ensembl.ensembl_prosite',
+              'ensembl.ensembl_interpro',
+
+              'pharmgkb',
+              'reporter',
+               ]
+
+__sources__ = [
+              'entrez.entrez_geneinfo',
+              'entrez.entrez_homologene',
+              'entrez.entrez_genesummary',
+              'entrez.entrez_accession',
+              'entrez.entrez_refseq',
+              'entrez.entrez_unigene',
+              'entrez.entrez_go',
+              'entrez.entrez_ec',
+              'entrez.entrez_retired',
+
+              'uniprot',
+              'uniprot.uniprot_pdb',
+              'uniprot.uniprot_ipi',
+              'uniprot.uniprot_pir',
+
+              'pharmgkb',
+              'reporter',
                ]
 
 
@@ -74,9 +107,16 @@ class GeneDocSource(Document):
     def load(self, genedoc_d=None, update_data=True, update_master=True, test=False, step=10000):
         if update_data:
             genedoc_d = genedoc_d or self.load_genedoc()
-            for doc in self.doc_iterator(genedoc_d, batch=False):
+
+            print "Uploading to the DB...",
+            t0 = time.time()
+            # for doc in self.doc_iterator(genedoc_d, batch=False):
+            #     if not test:
+            #         doc.save()
+            for doc_li in self.doc_iterator(genedoc_d, batch=True, step=step):
                 if not test:
-                    doc.save()
+                    self.collection.insert(doc_li, manipulate=False, check_keys=False)
+            print 'Done[%s]' % timesofar(t0)
         if update_master:
             #update src_master collection
             if not test:
@@ -86,6 +126,9 @@ class GeneDocSource(Document):
                 for attr in ['ENTREZ_GENEDOC_ROOT', 'ENSEMBL_GENEDOC_ROOT']:
                     if hasattr(self, attr):
                         _doc[attr] = getattr(self, attr)
+                if hasattr(self, 'get_mapping'):
+                    _doc['mapping'] = getattr(self, 'get_mapping')()
+
                 conn.GeneDocSourceMaster(_doc).save()
 
 
@@ -101,11 +144,19 @@ def register_sources():
 
 #register_sources()
 
+def load_src(src, **kwargs):
+    print "Loading %s..." % src
+    _src = conn[src+'_doc']()
+    _src.load(**kwargs)
+
+def update_mapping(src):
+    _src = conn[src+'_doc']()
+    _src.load(update_data=False, update_master=True)
+
 def load_all(**kwargs):
     for src in __sources__:
-        print "Loading %s..." % src
-        _src = conn[src+'_doc']()
-        _src.load(**kwargs)
+        load_src(src, **kwargs)
+
 
 def get_mapping():
     mapping = {}
