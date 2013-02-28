d3a2080565209de61ca256185b7e415ed4ba3638
newgene <anewgene@yahoo.com>
2013-02-27 16:35:11 -0800

    added optional "exclude_attrs" parameter for diff_doc function; allow to retrieve matching docs from two collections in batch.

--- src/utils/diff.py
+++ src/utils/diff.py
@@ -1,14 +1,18 @@
 '''
 Utils to compare two list of gene documents
 '''
+import time
 from databuild.backend import (GeneDocMongoDBBackend,
                                GeneDocCouchDBBackend)
+from utils.common import timesofar
 
-def diff_doc(doc_1, doc_2):
+def diff_doc(doc_1, doc_2, exclude_attrs=[]):
     diff_d = {'update': {},
               'delete': [],
               'add': {}}
     for attr in set(doc_1) | set(doc_2):
+        if exclude_attrs and attr in exclude_attrs:
+            continue
         if attr in doc_1 and attr in doc_2:
             _v1 = doc_1[attr]
             _v2 = doc_2[attr]
@@ -20,6 +24,21 @@ def diff_doc(doc_1, doc_2):
             diff_d['add'][attr] = doc_2[attr]
     if diff_d['update'] or diff_d['delete'] or diff_d['add']:
         return diff_d
+
+def two_docs_iterator(b1, b2, id_list, step = 10000):
+    t0 = time.time()
+    n = len(id_list)
+    for i in range(0, n, step):
+        t1 = time.time()
+        print "Processing %d-%d documents..." % (i+1, min(i+step, n)) ,
+        _ids = id_list[i:i+step]
+        iter1 = b1.mget_from_ids(_ids, asiter=True)
+        iter2 = b2.mget_from_ids(_ids, asiter=True)
+        for doc1, doc2 in zip(iter1, iter2):
+            yield doc1, doc2
+        print 'Done.[%.1f%%,%s]' % (i*100./n, timesofar(t1))
+    print "="*20
+    print 'Finished.[total time: %s]' % timesofar(t0)
 
 
 def diff_collections(b1, b2):
@@ -42,12 +61,14 @@ def diff_collections(b1, b2):
     print "# of docs found only in collection 2:\t", len(id_in_2)
     print "# of docs found in both collections:\t", len(id_common)
 
-    print "Comparing matching docs...",
+    print "Comparing matching docs..."
     _updates = []
     if len(id_common) > 0:
-        for _id in id_common:
-            doc1 = b1.get_from_id(_id)
-            doc2 = b2.get_from_id(_id)
+        # for _id in id_common:
+        #     doc1 = b1.get_from_id(_id)
+        #     doc2 = b2.get_from_id(_id)
+        for doc1, doc2 in two_docs_iterator(b1, b2, list(id_common)):
+            assert doc1['_id'] == doc2['_id']
             _diff = diff_doc(doc1, doc2)
             if _diff:
                 _diff['_id'] = _id
@@ -64,7 +85,7 @@ def diff_collections(b1, b2):
 
     changes = {'update': _updates,
                'delete': _deletes,
-               'add': '_adds'}
+               'add': _adds}
     return changes
 
 
