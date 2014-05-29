75b8c3fe874d193c6e830d494bedf6dd1717268d
karthik <karthik@localhost>
2014-05-29 11:15:19 -0700

    Added mouse and yeast data_file links.

--- src/dataload/sources/cpdb/__init__.py
+++ src/dataload/sources/cpdb/__init__.py
@@ -2,7 +2,9 @@ from cpdb_base import load_cpdb
 
 __metadata__ = {
     '__collection__' : 'cpdb',
-    '__url__': 'http://cpdb.molgen.mpg.de/CPDB/getPathwayGenes?idtype=entrez-gene'
+    '__url_human__': 'http://cpdb.molgen.mpg.de/CPDB/getPathwayGenes?idtype=entrez-gene',
+    '__url_mouse__': 'http://cpdb.molgen.mpg.de/MCPDB/getPathwayGenes?idtype=entrez-gene',
+    '__url_yeast__': 'http://cpdb.molgen.mpg.de/YCPDB/getPathwayGenes?idtype=entrez-gene'
 }
 
 
--- src/dataload/sources/cpdb/cpdb_base.py
+++ src/dataload/sources/cpdb/cpdb_base.py
@@ -8,30 +8,34 @@ DATA_FOLDER = get_data_folder('cpdb')
 def load_cpdb():
         
     print('DATA_FOLDER: '+ DATA_FOLDER)
-    DATA_FILE = os.path.join(DATA_FOLDER, 'CPDB_pathways_genes.tab')
-    load_start(DATA_FILE)
-    f= open(DATA_FILE,"r")
-    lines = f.readlines()
+    DATA_FILES = []
+    DATA_FILES.append(os.path.join(DATA_FOLDER, 'CPDB_pathways_genes_mouse.tab'))
+    DATA_FILES.append(os.path.join(DATA_FOLDER, 'CPDB_pathways_genes_yeast.tab'))
+    DATA_FILES.append(os.path.join(DATA_FOLDER, 'CPDB_pathways_genes_human.tab'))
     arr = {}
-    for line in lines:
-        line = line.rstrip('\n')
-        cols = line.split("\t")
-        genes = cols[len(cols)-1].split(",")
-        for gene in genes:
-            if gene != "entrez_gene_ids" and gene in arr.keys():
-                if cols[len(cols)-2] not in arr[gene]['pathway'].keys():
-                    arr[gene]['pathway'][cols[len(cols)-2].lower()]={'name':''}
-                arr[gene]['pathway'][cols[len(cols)-2].lower()]['name'] = cols[len(cols)-4]
-                if cols[len(cols)-3] != "None":
-                    if cols[len(cols)-2].lower() == "kegg":
-                        arr[gene]['pathway'][cols[len(cols)-2].lower()]['id'] = cols[len(cols)-3].replace("path:","")
-                    else :
-                        arr[gene]['pathway'][cols[len(cols)-2].lower()]['id'] = cols[len(cols)-3]
-            else:
-                if cols[len(cols)-3] != "None":
-                    arr[gene]= {'pathway':{cols[len(cols)-2].lower():{'name':cols[len(cols)-4], 'id': cols[len(cols)-3].replace("path:","")}}}
+    for DATA_FILE in DATA_FILES:
+        load_start(DATA_FILE)
+        f= open(DATA_FILE,"r")
+        lines = f.readlines()
+        for line in lines:
+            line = line.rstrip('\n')
+            cols = line.split("\t")
+            genes = cols[len(cols)-1].split(",")
+            for gene in genes:
+                if gene != "entrez_gene_ids" and gene in arr.keys():
+                    if cols[len(cols)-2] not in arr[gene]['pathway'].keys():
+                        arr[gene]['pathway'][cols[len(cols)-2].lower()]={'name':''}
+                    arr[gene]['pathway'][cols[len(cols)-2].lower()]['name'] = cols[len(cols)-4]
+                    if cols[len(cols)-3] != "None":
+                        if cols[len(cols)-2].lower() == "kegg":
+                            arr[gene]['pathway'][cols[len(cols)-2].lower()]['id'] = cols[len(cols)-3].replace("path:","")
+                        else :
+                            arr[gene]['pathway'][cols[len(cols)-2].lower()]['id'] = cols[len(cols)-3]
+                else:
+                    if cols[len(cols)-3] != "None":
+                        arr[gene]= {'pathway':{cols[len(cols)-2].lower():{'name':cols[len(cols)-4], 'id': cols[len(cols)-3].replace("path:","")}}}
 
-    load_done('[%d]' % len(arr))
+        load_done('[%d]' % len(arr))
 
     return arr
 
