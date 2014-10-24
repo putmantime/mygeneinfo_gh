279aa6a4e2f853b2ddf52c504bff2aa9e540a947
newgene <anewgene@yahoo.com>
2014-10-23 17:03:08 -0700

    downloading both hg38 and hg19 data files from ucsc.

--- src/dataload/data_dump/dl_ucsc.py
+++ src/dataload/data_dump/dl_ucsc.py
@@ -1,4 +1,4 @@
-#http://hgdownload.soe.ucsc.edu/goldenPath/hg19/database/
+# http://hgdownload.soe.ucsc.edu/goldenPath/hg19/database/
 
 # Copyright [2010-2013] [Chunlei Wu]
 #
@@ -29,14 +29,27 @@ from utils.mongo import get_src_dump
 from config import DATA_ARCHIVE_ROOT
 
 timestamp = time.strftime('%Y%m%d')
-#DATA_FOLDER=os.path.join(DATA_ARCHIVE_ROOT, 'by_resources/ucsc', timestamp)
-DATA_FOLDER=os.path.join(DATA_ARCHIVE_ROOT, 'by_resources/ucsc')
+# DATA_FOLDER=os.path.join(DATA_ARCHIVE_ROOT, 'by_resources/ucsc', timestamp)
+DATA_FOLDER = os.path.join(DATA_ARCHIVE_ROOT, 'by_resources/ucsc')
 
 FTP_SERVER = 'hgdownload.cse.ucsc.edu'
 BASE_PATH = 'goldenPath/currentGenomes'
-#DATAFILE_PATH = 'pub/databases/uniprot/current_release/knowledgebase/idmapping/idmapping_selected.tab.gz'
+# DATAFILE_PATH = 'pub/databases/uniprot/current_release/knowledgebase/idmapping/idmapping_selected.tab.gz'
 
-latest_lastmodified = None   #a global variable to record the lastmodified for the newest file.
+latest_lastmodified = None   # a global variable to record the lastmodified for the newest file.
+
+
+def get_ftpfile_lastmodified(ftp, file_path):
+    '''return lastmodified for a given file on ftp server.'''
+    try:
+        response = ftp.sendcmd('MDTM ' + file_path)
+    except error_perm:
+        return None
+    code, lastmodified = response.split()
+    # an example: 'last-modified': '20121128150000'
+    lastmodified = datetime.strptime(lastmodified, '%Y%m%d%H%M%S')
+    return lastmodified
+
 
 def get_file_list():
     ftp = FTP(FTP_SERVER)
@@ -46,14 +59,13 @@ def get_file_list():
     for genome in genome_li:
         for f in ['database/refFlat.txt.gz']:
             file_path = os.path.join(genome, f)
-            try:
-                response = ftp.sendcmd('MDTM ' + file_path)
-                code, lastmodified = response.split()
-                # an example: 'last-modified': '20121128150000'
-                lastmodified = datetime.strptime(lastmodified, '%Y%m%d%H%M%S')
-                fli.append((file_path, lastmodified))
-            except error_perm:
-                pass
+            fli.append(file_path)
+    # now add refFlat.txt.gz for hg38
+    file_path = 'goldenPath/hg38/database/refFlat.txt.gz'
+    fli.append(file_path)
+
+    fli = [(file_path, get_ftpfile_lastmodified(ftp, file_path)) for file_path in fli]
+    fli = [x for x in fli if x[1]]    # remove item if lastmodified is None
     ftp.close()
 
     if fli:
@@ -83,7 +95,7 @@ def download_ftp_file(url, outfile):
     with file(outfile, 'wb') as out_f:
         ftp.retrbinary('RETR %s' % url_parsed.path, out_f.write)
 
-    #set the mtime to match remote ftp server
+    # set the mtime to match remote ftp server
     response = ftp.sendcmd('MDTM ' + url_parsed.path)
     code, lastmodified = response.split()
     # an example: 'last-modified': '20121128150000'
@@ -103,7 +115,7 @@ def download(download_list=None, no_confirm=False):
                 if not os.path.exists(path):
                     os.makedirs(path)
                 os.chdir(path)
-                print 'Downloading "%s"...' % file_path ,
+                print 'Downloading "%s"...' % file_path,
                 url = 'ftp://{}/{}'.format(FTP_SERVER, file_path)
                 download_ftp_file(url, os.path.join(DATA_FOLDER, file_path))
                 print "done."
@@ -124,7 +136,7 @@ def download(download_list=None, no_confirm=False):
 
 
 if __name__ == '__main__':
-    no_confirm = True   #set it to True for running this script automatically without intervention.
+    no_confirm = True   # set it to True for running this script automatically without intervention.
 
     src_dump = get_src_dump()
     download_list = get_file_list_for_download()
@@ -139,12 +151,12 @@ if __name__ == '__main__':
     log_f, logfile = safewfile(os.path.join(DATA_FOLDER, 'ucsc_dump.log'), prompt=(not no_confirm), default='O')
     sys.stdout = LogPrint(log_f, timestamp=True)
 
-    #mark the download starts
+    # mark the download starts
     doc = {'_id': 'ucsc',
            'timestamp': timestamp,
            'data_folder': DATA_FOLDER,
            'lastmodified': latest_lastmodified,
-           'logfile':logfile,
+           'logfile': logfile,
            'status': 'downloading'}
     src_dump.save(doc)
     t0 = time.time()
@@ -152,10 +164,10 @@ if __name__ == '__main__':
         download(download_list, no_confirm)
     finally:
         sys.stdout.close()
-    #mark the download finished successfully
+    # mark the download finished successfully
     _updates = {
-                'status': 'success',
-                'time': timesofar(t0),
-                'pending_to_upload': True    # a flag to trigger data uploading
-                }
+        'status': 'success',
+        'time': timesofar(t0),
+        'pending_to_upload': True    # a flag to trigger data uploading
+    }
     src_dump.update({'_id': 'ucsc'}, {'$set': _updates})
