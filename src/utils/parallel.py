b4264a8e894bad621be37f870bc5ef6284e30b0b
newgene <anewgene@yahoo.com>
2015-09-09 16:41:06 -0700

    py3 compatibility

--- src/utils/parallel.py
+++ src/utils/parallel.py
@@ -1,13 +1,14 @@
 '''
 Utils for running parallel jobs on IPython cluster.
 '''
+from __future__ import print_function
 import time
-import types
 import copy
-from IPython.parallel import Client, require
+from IPython.parallel import Client
 
 from config import CLUSTER_CLIENT_JSON
 from utils.common import timesofar, ask
+
 
 def run_jobs_on_ipythoncluster(worker, task_list, shutdown_ipengines_after_done=False):
 
@@ -15,36 +16,37 @@ def run_jobs_on_ipythoncluster(worker, task_list, shutdown_ipengines_after_done=
     rc = Client(CLUSTER_CLIENT_JSON)
     lview = rc.load_balanced_view()
     cnt_nodes = len(lview.targets or rc.ids)
-    print "\t# nodes in use: {}".format(cnt_nodes)
+    print("\t# nodes in use: {}".format(cnt_nodes))
     lview.block = False
 
-    print "\t# of tasks: {}".format(len(task_list))
-    print "\tsubmitting...",
+    print("\t# of tasks: {}".format(len(task_list)))
+    print("\tsubmitting...", end='')
     job = lview.map_async(worker, task_list)
-    print "done."
+    print("done.")
     try:
         job.wait_interactive()
     except KeyboardInterrupt:
         #handle "Ctrl-C"
         if ask("\nAbort all submitted jobs?") == 'Y':
             lview.abort()
-            print "Aborted, all submitted jobs are cancelled."
+            print("Aborted, all submitted jobs are cancelled.")
         else:
-            print "Aborted, but your jobs are still running on the cluster."
+            print("Aborted, but your jobs are still running on the cluster.")
         return
 
     if len(job.result) != len(task_list):
-        print "WARNING:\t# of results returned ({}) != # of tasks ({}).".format(len(job.result), len(task_list))
-    print "\ttotal time: {}".format(timesofar(t0))
+        print("WARNING:\t# of results returned ({}) != # of tasks ({}).".format(len(job.result), len(task_list)))
+    print("\ttotal time: {}".format(timesofar(t0)))
 
     if shutdown_ipengines_after_done:
-        print "\tshuting down all ipengine nodes...",
+        print("\tshuting down all ipengine nodes...", end='')
         lview.shutdown()
-        print 'Done.'
+        print('Done.')
     return job.result
 
+
 def collection_partition(src_collection_list, step=100000):
-    if src_collection_list not in (types.ListType, types.TupleType):
+    if not isinstance(src_collection_list, (list, tuple)):
         src_collection_list = [src_collection_list]
 
     kwargs = {}
